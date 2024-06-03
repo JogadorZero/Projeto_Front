bb792e54831ca766da66285ce595d31fd806f19f
kaiquedm12 <160547555+kaiquedm12@users.noreply.github.com>
2024-06-03 20:12:33 -0300

    kaique2

--- json_server/src/components/Login.jsx
+++ json_server/src/components/Login.jsx
@@ -1,16 +1,18 @@
-import React, { useState } from "react"
+import React from "react"
+import './ProductForm.css'
+
 export default function Login({saveProduct,usuario, senha, handleSenha, handleUsuario}){
     return (
         <>
             <div className='container'>
-            <h2>Casdastro de Usuário</h2>
-            <form onSubmit={saveProduct}>
-                <label for="Nome">Usuario:</label>
-                <input type="text" name="Usuario" value={usuario} required onChange={(e) => handleUsuario(e)}/>
-                <label for="password">Senha:</label>
-                <input type="text" name="Senha" value={senha} required onChange={(e) => handleSenha(e)}/>
-                <input type="submit" value="Cadastrar"></input>
-            </form>
+        <h2>Cadastro de Produtos</h2>
+        <form onSubmit={(e) => saveProduct(e)}>
+            <label className='form-label' htmlFor="usuario">Usuario</label>
+            <input className='form-input' value={usuario} type="text" name="usuario" onChange={(e) => handleUsuario(e)} required/>
+            <label className='form-label' htmlFor="senha">Senha</label>
+            <input className='form-input' value={senha} type="text" name="senha" onChange={(e) => handleSenha(e)} required/>
+            <input className='form-submit' type="submit" value="Cadastrar" />
+        </form>
             </div>
         </>
     )
